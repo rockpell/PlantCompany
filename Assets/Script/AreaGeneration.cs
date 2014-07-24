ffd5ebe74c37f93dc1e58f98d2697788e8105992
rockpell <amthffk@naver.com>
2014-07-24 21:00:06 +0900

    1

---
+++ Assets/Script/AreaGeneration.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AreaGeneration : MonoBehaviour {
+
+	MapGeneration mg = new MapGeneration();
+	public Transform Capital1;
+	int [,] mapArr = new int[26, 26];
+
+	public struct Capital{
+		public int cx;
+		public int cy;
+	}
+
+	Capital [] cp = new Capital[3];
+	
+	void Start () {
+		mapArr = mg.NationalArea();
+		rand ();
+		capitalGeneration ();
+	}
+
+	void Update () {
+	
+	}
+
+	void rand(){
+		int nations = 0;
+		int x=0, y=0;
+		int gap1 = 0, gap2 = 0;
+		while(nations<3){
+			x = Random.Range (0,26);
+			y = Random.Range(0,26);
+		    if(mapArr[x,y] == 1 || mapArr[x,y] == 2){
+				cp[nations].cx = y;
+				cp[nations].cy = x;
+
+				for(int i=0; i<nations; i++){
+					gap1 = cp[i].cx + cp[i].cy;
+					gap2 = x + y;
+					if(gap2-gap1<7 && gap2-gap1>-7){
+						nations--;
+					}
+				}
+
+				nations++;
+			}
+		}
+	}
+
+	void capitalGeneration(){
+		for (int i=0; i<3; i++) {
+			Instantiate (Capital1, new Vector3 (cp[i].cx* 1.73F, 0, cp[i].cy*1.52F*-1), Quaternion.identity);
+		}
+	}
+}
---
+++ Assets/Script/CameraMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.Translate(-0.3f,0,0);
+		}
+		if(Input.GetKey(KeyCode.RightArrow)){
+			transform.Translate(0.3f,0,0);
+		}
+		if(Input.GetKey(KeyCode.UpArrow)){
+			transform.Translate(0,0.3f,0);
+		}
+		if(Input.GetKey(KeyCode.DownArrow)){
+			transform.Translate(0,-0.3f,0.0000000000000000000000000001f);
+		}
+	}
+}
