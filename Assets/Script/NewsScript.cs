945149515cebcb02ba1c50b7fa2f32693a81c6d1
rockpell <amthffk@naver.com>
2014-07-25 19:39:09 +0900

    UI제작중(5% 된듯?)

--- Assets/Script/NewsScript.cs
+++ Assets/Script/NewsScript.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class NewsScript : MonoBehaviour {
 
-	GUIText TopNews;
+	public GUIText TopNews;
 
 	// Use this for initialization
 	void Start () {
-	
+		TopNews.guiText.text ="a";
+		Instantiate (TopNews, new Vector3 (0.4F, 0.98F, 1), Quaternion.identity);
 	}
 	
 	// Update is called once per frame
