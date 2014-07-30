48aae9f90bb9ca2013f1e5383980606b16df4a09
rockpell <amthffk@naver.com>
2014-07-30 19:11:38 +0900

    카메라 마우스 이동

--- Assets/Script/CameraMove.cs
+++ Assets/Script/CameraMove.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class CameraMove : MonoBehaviour {
 
+	public Transform MainCamera;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,17 +12,63 @@ public class CameraMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(KeyCode.LeftArrow)){
-			transform.Translate(-0.3f,0,0);
-		}
+
+		if(MainCamera.position.x<28){ // 카메라 오른쪽 이동
 		if(Input.GetKey(KeyCode.RightArrow)){
-			transform.Translate(0.3f,0,0);
+			transform.Translate(0.3f,0,0,Space.World);
 		}
+		if(Input.GetMouseButton(0)){ 
+			if(Input.GetAxis("Mouse X") < 0){ 
+				transform.Translate(0.3f, 0, 0 ,Space.World); 
+			} 
+		}
+		}
+
+
+		if(MainCamera.position.x>15){ // 카메라 왼쪽 이동
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.Translate(-0.3f,0,0,Space.World);
+		}
+		if(Input.GetMouseButton(0)){ 
+			if(Input.GetAxis("Mouse X") > 0){ 
+				transform.Translate(-0.3f, 0, 0 ,Space.World); 
+			} 
+		}
+		}
+
+		if(MainCamera.position.z<-12){ // 카메라 위로 이동
 		if(Input.GetKey(KeyCode.UpArrow)){
-			transform.Translate(0,0.3f,0);
+			transform.Translate(0,0,0.3f,Space.World);
 		}
+		if(Input.GetMouseButton(0)){ 
+			if(Input.GetAxis("Mouse Y") < 0){ 
+				transform.Translate(0, 0, 0.3f ,Space.World); 
+			} 
+		}
+		}
+
+		if(MainCamera.position.z>-38){ // 카메로 아래로 이동
 		if(Input.GetKey(KeyCode.DownArrow)){
-			transform.Translate(0,-0.3f,0.0000000000000000000000000001f);
+			transform.Translate(0,0,-0.3f,Space.World);
 		}
-	}
+		if(Input.GetMouseButton(0)){ 
+			if(Input.GetAxis("Mouse Y") > 0){ 
+				transform.Translate(0, 0, -0.3f ,Space.World); 
+			} 
+		}
+		}
+	
+	if (MainCamera.position.y < 50) {
+						if (Input.GetAxis ("Mouse ScrollWheel") < 0) {
+								transform.Translate (0, 0.5f, 0, Space.World); 
+						}
+				}
+
+	if (MainCamera.position.y > 10) {
+						if (Input.GetAxis ("Mouse ScrollWheel") > 0) { 
+								transform.Translate (0, -0.5f, 0, Space.World); 
+						}
+				}
+
+		}
 }
